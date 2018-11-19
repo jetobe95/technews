1ebdbeb030046cd30c3187660c9138f10d504267
Donayd Correa <donaydc@uninorte.edu.co>
2018-11-19 17:48:35 -0500

    Subiendo las imagenes

---
+++ src/scenes/App/Normal-user/Categories/components/categories/components/datoscategorias.js
@@ -0,0 +1,242 @@
+const squares = [
+  {
+    id: 1,
+    title: "Aplicación",
+    subtitle: "Lo mas reciente",
+    q: "aplicación",
+    image: require("./images/app.jpg"),
+    visible: true
+  },
+  {
+    id: 2,
+    title: "Android",
+    subtitle: "Lo mas reciente",
+    q: "android",
+    image: require("./images/android.jpg"),
+    visible: true
+  },
+  {
+    id: 3,
+    title: "Avances Tecnológicos",
+    subtitle: "Lo mas reciente",
+    q: "avances",
+    image: require("./images/avancestecnológicos.png"),
+    visible: true
+  },
+  {
+    id: 4,
+    title: "Celulares",
+    subtitle: "Lo mas reciente",
+    q: "celulares",
+    image: require("./images/Celulares.jpg"),
+    visible: true
+  },
+  {
+    id: 5,
+    title: "Ciberseguridad",
+    subtitle: "Lo mas reciente",
+    q: "ciberseguridad",
+    image: require("./images/ciberseguridad.jpg"),
+    visible: true
+  },
+  {
+    id: 6,
+    title: "Computacion",
+    subtitle: "Lo mas reciente",
+    q: "computacion",
+    image: require("./images/computacion.jpg"),
+    visible: true
+  },
+  {
+    id: 7,
+    title: "Deporte",
+    subtitle: "Lo mas reciente",
+    q: "deporte",
+    image: require("./images/deporte.jpg"),
+    visible: true
+  },
+  {
+    id: 8,
+    title: "Empresas",
+    subtitle: "Lo mas reciente",
+    q: "empresas",
+    image: require("./images/empresas.jpg"),
+    visible: true
+  },
+  {
+    id: 9,
+    title: "Energía",
+    subtitle: "Lo mas reciente",
+    q: "energía",
+    image: require("./images/energia.jpg"),
+    visible: true
+  },
+  {
+    id: 10,
+    title: "Facebook",
+    subtitle: "Lo mas reciente",
+    q: "facebook",
+    image: require("./images/facebook.jpg"),
+    visible: true
+  },
+  {
+    id: 11,
+    title: "Google",
+    subtitle: "Lo mas reciente",
+    q: "google",
+    image: require("./images/google.jpg"),
+    visible: true
+  },
+  {
+    id: 12,
+    title: "Imagenes 3D",
+    subtitle: "Lo mas reciente",
+    q: "3d",
+    image: require("./images/imagenes3d.jpg"),
+    visible: true
+  },
+  {
+    id: 13,
+    title: "Ingeniería",
+    subtitle: "Lo mas reciente",
+    q: "ingeniería",
+    image: require("./images/ingeniería.jpg"),
+    visible: true
+  },
+  {
+    id: 14,
+    title: "Inteligencia Artificial",
+    subtitle: "Lo mas reciente",
+    q: "inteligencia artificial",
+    image: require("./images/inteligenciaartificial.jpg"),
+    visible: true
+  },
+  {
+    id: 15,
+    title: "Internet",
+    subtitle: "Lo mas reciente",
+    q: "internet",
+    image: require("./images/internet.png"),
+    visible: true
+  },
+  {
+    id: 16,
+    title: "IOS",
+    subtitle: "Lo mas reciente",
+    q: "ios",
+    image: require("./images/ios.jpg"),
+    visible: true
+  },
+  {
+    id: 17,
+    title: "Machine Learning",
+    subtitle: "Lo mas reciente",
+    q: "machine learning",
+    image: require("./images/machinelearning.jpg"),
+    visible: true
+  },
+  {
+    id: 18,
+    title: "Medicina",
+    subtitle: "Lo mas reciente",
+    q: "medicina",
+    image: require("./images/medicina.jpg"),
+    visible: true
+  },
+  {
+    id: 19,
+    title: "Realidad Aumentada",
+    subtitle: "Lo mas reciente",
+    q: "realidad aumentada",
+    image: require("./images/realidadaumentada.jpg"),
+    visible: true
+  },
+  {
+    id: 20,
+    title: "Realidad Virtual",
+    subtitle: "Lo mas reciente",
+    q: "realidad virtual",
+    image: require("./images/realidadvirtual.jpg"),
+    visible: true
+  },
+  {
+    id: 21,
+    title: "Redes Sociales",
+    subtitle: "Lo mas reciente",
+    q: "redes sociales",
+    image: require("./images/redes.jpg"),
+    visible: true
+  },
+  {
+    id: 22,
+    title: "Robotica",
+    subtitle: "Lo mas reciente",
+    q: "robotica",
+    image: require("./images/robotica.jpg"),
+    visible: true
+  },
+  {
+    id: 23,
+    title: "Tecnologia",
+    subtitle: "Lo mas reciente",
+    q: "tecnologia",
+    image: require("./images/tecnologia.jpg"),
+    visible: true
+  },
+  {
+    id: 24,
+    title: "Televisión",
+    subtitle: "Lo mas reciente",
+    q: "televisión",
+    image: require("./images/television.jpg"),
+    visible: true
+  },
+  {
+    id: 25,
+    title: "Transporte",
+    subtitle: "Lo mas reciente",
+    q: "transporte",
+    image: require("./images/transporte.png"),
+    visible: true
+  },
+  {
+    id: 26,
+    title: "Videojuegos",
+    subtitle: "Lo mas reciente",
+    q: "Videojuegos",
+    image: require("./images/videojuegos.jpg"),
+    visible: true
+  },
+  {
+    id: 27,
+    title: "Whatsapp",
+    subtitle: "Lo mas reciente",
+    q: "whatsapp",
+    image: require("./images/whatsapp.jpg"),
+    visible: true
+  },
+  {
+    id: 28,
+    title: "Windows",
+    subtitle: "Lo mas reciente",
+    q: "windows",
+    image: require("./images/windows.jpg"),
+    visible: true
+  },
+  {
+    id: 29,
+    title: "Youtube",
+    subtitle: "Lo mas reciente",
+    q: "youtube",
+    image: require("./images/youtube.jpg"),
+    visible: true
+  },
+  {
+    id: 30,
+    title: "Canal Uninorte",
+    subtitle: "Lo mas reciente",
+    q: "canal",
+    image: require("./images/uninorte.jpg"),
+    visible: true
+  }
+];
